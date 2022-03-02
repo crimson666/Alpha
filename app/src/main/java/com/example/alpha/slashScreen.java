fa2ff54998e9cd3c49431baafca1d5c1f24b3f96
crimson666 <ankurbsws@gmail.com>
2022-03-03 02:07:19 +0530

    3nd Version Commit

---
+++ app/src/main/java/com/example/alpha/slashScreen.java
@@ -0,0 +1,14 @@
+package com.example.alpha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class slashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slash_screen);
+    }
+}
